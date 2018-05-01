b5c91e836e9cd096cddfc5491d95fd333fb24ff5
jordanpeshev <danypeshev@gmail.com>
2018-05-01 14:46:17 +0200

    no message

--- src/Physics/Physics.java
+++ src/Physics/Physics.java
@@ -1,45 +1,57 @@
 package Physics;
 
-
-
-
-
-public class Physics{
+public class Physics {
 
   final public static int delay = 1;
   public static double totalVelocity, angle;
   public static double xVelocity, yVelocity, zVelocity;
   public static double xCoordinate = 0, yCoordinate = 0, zCoordinate = 0;
   public static double xChange, yChange, zChange;
-  public static final double MASS_OF_BALL = 0.45;
+  public static final double MASS_OF_BALL = 0.045;
   public static double gravity = -9.81;
-  public static double slopeAngleX = 0, slopeAngleY = 0;
-  public static double leg;
+  public static double slopeAngleX, slopeAngleY;
+  
   public static PhysicsTimer timerlistener;
 
   public Physics(double totalVelocity, double angle){
       this.totalVelocity = totalVelocity;
-      this.angle = -angle;
+      this.angle = angle;
 
   }
     public void startMoving(){
         xCoordinate = 0;
         yCoordinate = 0;
         zCoordinate = 0;
-        System.out.println("TOTAL VELOCITY = " + totalVelocity + "m/s");
-        System.out.println("ANGLE OF SHOT = " + angle + "Â°");
         timerlistener = new PhysicsTimer(totalVelocity, angle);
-        zVelocity= (totalVelocity*Math.sin(timerlistener.getSlopeAngle(timerlistener.getXSlope(xCoordinate))) + totalVelocity*Math.sin(timerlistener.getSlopeAngle(timerlistener.getYSlope(yCoordinate))));
-        leg=Math.sqrt((totalVelocity*totalVelocity)-(zVelocity*zVelocity));
-        xVelocity=leg*Math.sin(angle);
-        yVelocity=leg*Math.cos(angle);
+        double xPercentage = (10/9)*(Math.abs(angle));
+        double yPercentage = 100-xPercentage;
+        double angledSlope = ((xPercentage/100)*(getXSlope(xCoordinate, yCoordinate)))+((yPercentage/100)*(getYSlope(xCoordinate, yCoordinate)));
+        double angledSlopeAngle = getSlopeAngle(angledSlope);
+        double leg = totalVelocity*Math.cos(angledSlopeAngle);
+        xVelocity = leg*Math.sin(angle)/1;
+        yVelocity = leg*Math.cos(angle)/1;
+        zVelocity = totalVelocity*Math.sin(angledSlopeAngle)/100;
         timerlistener.start();
     }
-  public static double radiansToDegrees(double degree){
-    return ((degree*Math.PI)/180);
-  }
-  public void stop(){
+
+  public void stop()
+  {
       timerlistener.stop();
+  }
+
+  public double getXSlope(double xCoordinate, double yCoordinate)
+  {
+    return (0.1 + (0.06*xCoordinate));
+  }
+
+  public double getYSlope(double xCoordinate, double yCoordinate)
+  {
+    return 0.2;
+  }
+
+  public double getSlopeAngle(double slope)
+  {
+    return Math.atan(slope);
   }
 
 }
--- src/Physics/PhysicsTimer.java
+++ src/Physics/PhysicsTimer.java
@@ -1,12 +1,8 @@
 package Physics;
-//import java.util.Scanner;
+
 import Graphics.Graph3D;
 import java.util.Timer;
-//import javax.swing.event.*;
-//import java.awt.*;
-//import java.awt.event.*;
 import java.util.TimerTask;
-//import java.awt.Graphics;
 
 public class PhysicsTimer extends Physics{
   public double newVelocity;
@@ -14,42 +10,66 @@ public class PhysicsTimer extends Physics{
   public double gravityForceOnZ_X = 0, gravityForceOnZ_Y = 0;
   public double gravityForceOnBallX = 0, gravityForceOnBallY = 0;
   public double oldX, oldY, oldZ;
+  public double calculatedAngleX;
+  public double calculatedAngleY;
 
   Timer timer = new Timer();
-  TimerTask task = new TimerTask(){ 
-    @Override
+  TimerTask task = new TimerTask(){
     public void run(){
 
-      slopeAngleX = getSlopeAngle(getXSlope(xCoordinate));
-      slopeAngleY = getSlopeAngle(getYSlope(yCoordinate));
+        slopeAngleX = getSlopeAngle(getXSlope(xCoordinate, yCoordinate));
+        slopeAngleY = getSlopeAngle(getYSlope(xCoordinate, yCoordinate));   
+        
+        Graph3D.moveBall(xCoordinate, zCoordinate, yCoordinate);
 
-       
-      Graph3D.moveBall(xCoordinate, zCoordinate, yCoordinate);
+      System.out.println("X Coordinate = " + xCoordinate);
+      System.out.println("Y Coordinate: " + yCoordinate);
+      System.out.println("Z Coordinate: " + zCoordinate);
+      System.out.println("X VELOCITY = " + xVelocity);
+      System.out.println("Y VELOCITY = " + yVelocity);
+      System.out.println("");
 
-      gravityForceOnBallX = (gravity*MASS_OF_BALL)*Math.sin(slopeAngleX);
-      gravityForceOnBallY = (gravity*MASS_OF_BALL)*Math.sin(slopeAngleY);
-      //gravityForceOnX = gravityForceOnBallX;
-      //gravityForceOnY = gravityForceOnBallY;
-      gravityForceOnX = (gravityForceOnBallX)*Math.sin(slopeAngleX);
+      //oldZ = zVelocity;
+      oldY = yVelocity;
+      oldX = xVelocity;
+
+      if (slopeAngleX >= 0)
+      {
+        calculatedAngleX = slopeAngleX;
+      }
+      else
+      {
+        calculatedAngleX = 90-slopeAngleX;
+      }
+
+      if (slopeAngleY >= 0)
+      {
+        calculatedAngleY = slopeAngleY;
+      }
+      else
+      {
+        calculatedAngleY = 90-slopeAngleY;
+      }
+
+      gravityForceOnBallX = (gravity*MASS_OF_BALL)*Math.sin(calculatedAngleX);
+      gravityForceOnBallY = (gravity*MASS_OF_BALL)*Math.sin(calculatedAngleY);
+      gravityForceOnX = (gravityForceOnBallX)*Math.cos(slopeAngleX);
       gravityForceOnY = (gravityForceOnBallY)*Math.cos(slopeAngleY);
 
-      gravityForceOnZ_X = (gravityForceOnBallX)*Math.cos(slopeAngleX);
-      gravityForceOnZ_Y = (gravityForceOnBallY)*Math.cos(slopeAngleY);
+      //gravityForceOnZ_X = (gravityForceOnBallX)*Math.cos(slopeAngleX);
+      //gravityForceOnZ_Y = (gravityForceOnBallY)*Math.cos(slopeAngleY);
 
-      gravityForceOnZ = gravityForceOnZ_X + gravityForceOnZ_Y;
+      //gravityForceOnZ = gravityForceOnZ_X + gravityForceOnZ_Y;
 
-      oldZ = zVelocity;
-      zVelocity = ((zVelocity + getAcceleration(gravityForceOnZ)*delay) - 0.1*(zVelocity + getAcceleration(gravityForceOnZ)*delay));
-      oldY = yVelocity;
-      yVelocity = ((yVelocity + getAcceleration(gravityForceOnY)*delay) - 0.1*(yVelocity + getAcceleration(gravityForceOnY)*delay));
-      oldX = xVelocity;
-      xVelocity = ((xVelocity + getAcceleration(gravityForceOnX)*delay) - 0.1*(xVelocity + getAcceleration(gravityForceOnX)*delay));
+      //zVelocity = ((zVelocity + getAcceleration(gravityForceOnZ)*delay) - 0.1*(zVelocity + getAcceleration(gravityForceOnZ)*delay));
+      yVelocity = (((yVelocity + getAcceleration(gravityForceOnY)*delay) - 0*(yVelocity + getAcceleration(gravityForceOnY)*delay))/1);
+      xVelocity = (((xVelocity + getAcceleration(gravityForceOnX)*delay) - 0*(xVelocity + getAcceleration(gravityForceOnX)*delay))/1);
 
-      xChange=((xVelocity+oldX)*delay)/2;
-      xCoordinate += xChange/5000;
-      yChange=((yVelocity+oldY)*delay)/2;
-      yCoordinate += yChange/5000;
-      zCoordinate = 0.1*(xCoordinate)+0.03*(xCoordinate*xCoordinate)+0.2*(yCoordinate);
+      xChange=((xVelocity+oldX)*1)/2;
+      xCoordinate += xChange/999999;
+      yChange=((yVelocity+oldY)*1)/2;
+      yCoordinate += yChange/999999;
+      zCoordinate = 0.1*((xCoordinate)+0.03*(Math.pow(xCoordinate, 2)) + 0.2*(yCoordinate));
     }
   };
 
@@ -57,28 +77,16 @@ public class PhysicsTimer extends Physics{
         super(totalVelocity, angle);
    }
 
-
   public void start(){
     timer.scheduleAtFixedRate(task, 1, delay);
   }
 
-  public double getSlopeAngle(double slope){
-    return radiansToDegrees(Math.abs(Math.atan(slope)));
-  }
-
   public double getAcceleration(double force){
-    return force/MASS_OF_BALL; //in m*s^2
+    return force/MASS_OF_BALL;
   }
 
-  public double getXSlope(double xCoordinate){
-    return (0.1 + (0.06*xCoordinate));
-  }
-
-  public double getYSlope(double yCoordinate){
-    return 0.2;
-  }
   public void stop(){
       timer.cancel();
   }
-  
+
 }
